da0735b984cf5c4322819d052a1c3fb559fe93db
Aaditya0109 <129144512+Aaditya20020109@users.noreply.github.com>
2023-12-07 23:38:10 +0530

    Time: 1173 ms (5.33%) | Memory: 71.9 MB (9.79%) - LeetSync

--- 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.java
+++ 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.java
@@ -1,48 +1,34 @@
 class Solution {
 
-    public int maxArea(int a[]) {
-        Arrays.sort(a);
-        int i = a.length - 1;
-
-        for (int e : a) System.out.print(e);
-        System.out.println();
+    public int maxArea(int height[]) {
+        Arrays.sort(height);
         int max = 0;
-        while (i >= 0) {
-            if (i != a.length - 1) {
-                int currPos = i;
-                int diff = a.length - currPos;
-                int base = diff * a[i];
-                a[i] = base; //base
-            }
-            max = Math.max(a[i], max);
-            i--;
+        for (int j = 0; j < height.length; j++) {
+            int base = height.length - j - 1;
+            int area = (height[j] + (height[j] * base));
+            System.out.print(area);
+            max = Math.max(area, max);
         }
         return max;
     }
 
     public int largestSubmatrix(int[][] matrix) {
-        int rowLen = matrix.length;
-        int colLen = matrix[0].length;
-        int heightMatrix[][] = new int[rowLen][colLen];
-        int Max = 0;
-        //write the first row into heightMatrix because it has no height.
-        for (int i = 0; i < colLen; i++) {
-            heightMatrix[0][i] = matrix[0][i];
-        }
-
-        //start from the second row and add the previous  ones height with current ones.
-        for (int i = 1; i < rowLen; i++) {
-            for (int j = 0; j < colLen; j++) {
-                if (matrix[i][j] != 0) {
-                    heightMatrix[i][j] = matrix[i][j] + heightMatrix[i - 1][j]; //current + (previous Row element height)
-                } else heightMatrix[i][j] = 0;
+        int n = matrix.length;
+        int i = 1;
+        while (i < n) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 1 && matrix[i - 1][j] >= 1) {
+                    matrix[i][j] += matrix[i - 1][j];
+                }
             }
+
+            i++;
         }
 
-        for (int i = 0; i < rowLen; i++) {
-            int mat[] = heightMatrix[i];
-            Max = Math.max(maxArea(mat), Max);
+        int max = 0;
+        for (int k = 0; k < n; k++) {
+            max = Math.max(max, maxArea(matrix[k]));
         }
-        return Max;
+        return max;
     }
 }
