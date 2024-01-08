123b82cb457f316975cf68c031adae14aaf33ed8
Aaditya0109 <129144512+Aaditya20020109@users.noreply.github.com>
2024-01-08 21:43:06 +0530

    Time: 12 ms (32.01%) | Memory: 7.6 MB (78.74%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()!=t.size())return false;
-       map<char,int> m;
-       for(char &ch:s)
-       {
-           if(m.find(ch)!=m.end())
-           {
-               m[ch] = m[ch]+1;
-           }
-           else 
-           {
-               m.insert({ch,1});
+    //     if(s.size()!=t.size())return false;
+    //    map<char,int> m;
+    //    for(char &ch:s)
+    //    {
+    //        if(m.find(ch)!=m.end())
+    //        {
+    //            m[ch] = m[ch]+1;
+    //        }
+    //        else 
+    //        {
+    //            m.insert({ch,1});
 
-           }
-       }
+    //        }
+    //    }
 
-       for(char &ch:t)
-       {
-            if(m.find(ch)!=m.end())
-           {
-               m[ch] = m[ch]-1;
-               if(m[ch]==0)m.erase(ch);
-           }
-           else 
-           {
-               return false;
-           }
-       }
-       return true;
+    //    for(char &ch:t)
+    //    {
+    //         if(m.find(ch)!=m.end())
+    //        {
+    //            m[ch] = m[ch]-1;
+    //            if(m[ch]==0)m.erase(ch);
+    //        }
+    //        else 
+    //        {
+    //            return false;
+    //        }
+    //    }
+    //    return true;
         
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return (s==t);
     }
 };
