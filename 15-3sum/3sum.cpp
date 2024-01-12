7007b326dab69eb6b24e841042f51396e3fb485f
Aaditya0109 <129144512+Aaditya20020109@users.noreply.github.com>
2024-01-12 22:19:02 +0530

    Time: 128 ms (60.14%) | Memory: 27.2 MB (45.91%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,31 +1,40 @@
+#include <vector>
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-         vector<vector<int>> res;
-    sort(nums.begin(),nums.end());
-    int i=0;
-    for(i=0;i<nums.size()-2;i++)
-    {
-        if(i>0 && nums[i]==nums[i-1])continue;
-        int j=i+1,k=nums.size()-1;
-        while(j<k)
-        {
-              int sum = nums[i]+nums[j]+nums[k];
-             if(sum==0) {
-                 vector<int> v = {nums[i],nums[j++],nums[k--]};
-                res.push_back(v);
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
+        int i = 0;
+        int n = nums.size();
+        for (i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            else {
+                int j = i + 1;
+                int k = n - 1;
+                while (j < k) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (sum == 0) {
+                        vector<int> v = {nums[i], nums[j], nums[k]};
+                        if (res.size() > 0) {
+                            int m = res.size();
+                            if (res[m - 1] != v) {
+                                res.push_back(v);
+                            }
+                        } else {
+                            res.push_back(v);
+                        }
 
-                // remove duplicate 
-                while(j<k && nums[j]==nums[j-1])j++;
-                while(j<k && nums[k]==nums[k+1])k--;
-                
-             }
-              else if(sum>0)k--;
-              else if(sum<0)j++;
-               
-              
+                        k--;
+                        j++;
+                    }
+                    if (sum > 0)
+                        k--;
+                    if (sum < 0)
+                        j++;
+                }
+            }
         }
-    }
-    return res;
+        return res;
     }
 };
