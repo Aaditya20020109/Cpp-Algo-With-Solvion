48f73b7a439de1cb53521d8374089803614d70dc
Aaditya0109 <129144512+Aaditya20020109@users.noreply.github.com>
2024-02-05 12:45:12 +0530

    Time: 19 ms (90.75%) | Memory: 11.7 MB (31.20%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        map<char ,int> m;
-        for(char &ch:s)
-        {
-            if(m.find(ch)!=m.end())m[ch]++;
-            else m.insert({ch,1});
-        }
+       vector<int> freq(26,0);
+        
+        for(char &ch:s)freq[ch-'a']++;
+
         for(int i=0;i<s.size();i++)
         {
-            if(m[s[i]]==1)return i;
+            if(freq[s[i]-'a']==1)return i;
         }
         return -1;
         
