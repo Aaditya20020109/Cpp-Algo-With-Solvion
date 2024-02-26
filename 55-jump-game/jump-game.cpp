09fe1d8ff0a935fd06db107641bc5acf3a382e32
Aaditya0109 <129144512+Aaditya20020109@users.noreply.github.com>
2024-02-26 06:42:34 +0530

    Time: 74 ms (27.12%) | Memory: 50.9 MB (37.52%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,25 +1,55 @@
 class Solution {
 public:
-    int dp[100007];
-    
-    bool solve(int index, vector<int>& nums) {
-        if (dp[index] != -1)
-            return (dp[index] == 0) ? false : true;
-        if (index >= nums.size() - 1)
-            return true;
+    // int dp[];
+    // time complexity : O(n^k)exponential solution
+    // top-down approach
+    // bool solve(int index, vector<int>& nums) {
+    //     if (dp[index] != -1)
+    //         return (dp[index] == 0) ? false : true;
+    //     if (index >= nums.size() - 1)
+    //         return true;
 
-        for (int jump = 1; jump <= nums[index]; jump++) {
+    //     for (int jump = 1; jump <= nums[index]; jump++) {
 
-            if (solve(index + jump, nums)) {
-                dp[index] = 1;
-                return true;
-            }
-        }
-        dp[index] = 0;
-        return false;
-    }
+    //         if (solve(index + jump, nums)) {
+    //             dp[index] = 1;
+    //             return true;
+    //         }
+    //     }
+    //     dp[index] = 0;
+    //     return false;
+    // }
     bool canJump(vector<int>& nums) {
-        memset(dp, -1, sizeof(dp));
-        return solve(0, nums);
+        // memset(dp, -1, sizeof(dp));
+        // return solve(0, nums);
+        int n = nums.size();
+        bool dp[n];
+        memset(dp,false,sizeof(dp));
+        dp[0] = true;
+        for(int i=1;i<n;i++)
+        {
+            int j=i-1;
+            while(j>=0)
+            {
+                if(dp[j]==true && j+nums[j]>=i)
+                {
+                    dp[i] = true;
+                    break;
+                }
+                j--;
+            }
+
+        }
+        return dp[n-1];
+
+
+
+
+
+
+        // bottom approach
+
+
+
     }
 };
