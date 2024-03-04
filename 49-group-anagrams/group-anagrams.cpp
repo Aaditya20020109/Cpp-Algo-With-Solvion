4a169a987b3cff295c68be93ba41b49875755452
Aaditya0109 <129144512+Aaditya20020109@users.noreply.github.com>
2024-03-05 00:15:31 +0530

    Time: 40 ms (18.34%) | Memory: 24.9 MB (27.77%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    vector<vector<string>> Ans;
+    map<string,vector<string>> m;
+    for(string str:strs)
+    {
+        string value = str;
+        sort(begin(str),end(str));
+        if(m.find(str)!=m.end())
+        {
+            m[str].push_back(value);
 
-        map<string,vector<string>> m;
-        for (string s : strs) {
-            string str = s;
-            sort(s.begin(), s.end());
-            if (m.find(s) != m.end()) {
-                m[s].push_back(str);
-
-            } else {
-                vector<string> v = {str};
-                m.insert({s, {str}});
-            }
         }
-
-        vector<vector<string>> ans;
-        map<string, vector<string>>::iterator it;
-        for (it = m.begin(); it != m.end(); it++) {
-            ans.push_back(it->second);
+        else 
+        {
+            m.insert({str,{value}});
         }
-        return ans;
+    }
+
+    map<string,vector<string>>:: iterator it;
+    for(it=m.begin();it!=m.end();it++)
+    {
+        Ans.push_back(it->second);
+    }
+    return Ans;
+        
     }
 };
