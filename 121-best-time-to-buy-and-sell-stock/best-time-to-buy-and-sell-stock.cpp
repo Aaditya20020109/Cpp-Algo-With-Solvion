32ccd370035df9b1c91d71750e6f87465350d361
Aaditya0109 <129144512+Aaditya20020109@users.noreply.github.com>
2024-03-14 16:58:03 +0530

    Time: 82 ms (78.02%) | Memory: 96.7 MB (5.73%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -2,25 +2,27 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<int> left(n),right(n);
+        int left[n];
+        int right[n];
+        memset(left,0,sizeof(left));
+        memset(right,0,sizeof(right));
         left[0] = prices[0];
         right[n-1] = prices[n-1];
-        for(int i =1;i<n;i++)
+        for(int i=1;i<n;i++)
         {
-            left[i] = min(prices[i],left[i-1]);
+            left[i] = min(left[i-1],prices[i]);
         }
-
+        
         for(int i=n-2;i>=0;i--)
         {
-            right[i] = max(prices[i],right[i+1]);
+            right[i] = max(right[i+1],prices[i]);
         }
-        int maxi =0;
+        int max_profit = 0;
         for(int i=0;i<n;i++)
         {
-            maxi = max(abs(right[i]-left[i]),maxi);
-
+            int profit = abs(right[i]-left[i]);
+            max_profit = max(profit,max_profit);
         }
-        return maxi;
-        
+        return max_profit;
     }
 };
