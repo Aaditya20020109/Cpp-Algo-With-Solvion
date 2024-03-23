8c1b12167efab83fbf43ef1e4b62bc4f1b3c0795
Aaditya0109 <129144512+Aaditya20020109@users.noreply.github.com>
2024-03-23 06:43:21 +0530

    Time: 2 ms (88.69%) | Memory: 48.4 MB (22.58%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -1,51 +1,55 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-    public ListNode middleNode(ListNode head)
-    {
-        ListNode fast = head.next,slow = head;
-        while(fast!=null && fast.next!=null)
-        {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head.next, slow = head;
+        while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
         }
         return slow;
     }
+
+    public ListNode reverse(ListNode head) {
+        ListNode curr = head;
+        ListNode next = null, prev = null;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+
+        }
+        return prev;
+    }
+
     public void reorderList(ListNode head) {
-        ListNode mid =  middleNode(head);
+        ListNode mid = middleNode(head);
         ListNode head2 = mid.next;
         mid.next = null;
-        Stack<ListNode> st = new Stack();
-        while(head2!=null)
-        {
-            st.push(head2);
-            head2 = head2.next;
-        }
+        head2 = reverse(head2);
 
         ListNode curr = head;
         ListNode next = curr.next;
-        while(!st.isEmpty())
-        {
-            curr.next = st.peek();
-            st.pop();
+        while (head2 != null) {
+            curr.next = head2;
+            head2 = head2.next;
             curr = curr.next;
             curr.next = next;
-           if(next!=null)
-           {
-             curr = curr.next;
-            next = curr.next;
-           }
+            if (next != null) {
+                curr = curr.next;
+                next = curr.next;
+            }
+
         }
 
-     
-        
     }
 }
