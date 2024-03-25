ae9493e79ae66d51c728bacf9439f04bb702b3d0
Aaditya0109 <129144512+Aaditya20020109@users.noreply.github.com>
2024-03-25 12:35:44 +0530

    Time: 5 ms (31.39%) | Memory: 9 MB (58.57%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-    void Solve(vector<int> nums,vector<vector<int>>& res,vector<int>& temp,unordered_set<int> s)
-    // TC: O(n*n!)
+vector<vector<int>> res;
+    void Solve(int idx,vector<int>& nums)
     {
-        if(temp.size()==nums.size())
+        if(idx==nums.size())
         {
-            res.push_back(temp);
+            res.push_back(nums);
             return;
         }
-
-        for(int i=0;i<nums.size();i++)
+        for(int i=idx;i<nums.size();i++)
         {
-            if(s.find(nums[i])==s.end())
-            {
-                temp.push_back(nums[i]);
-                s.insert(nums[i]);
-                Solve(nums,res,temp, s);
-                temp.pop_back();
-                s.erase(nums[i]);
-            }
+            swap(nums[i],nums[idx]);
+            Solve(idx+1,nums);
+            swap(nums[i],nums[idx]);
+
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        unordered_set<int> s;
-        vector<int> temp;
-        vector<vector<int>> res;
-        Solve(nums,res,temp,s);
+        Solve(0,nums);
         return res;
 
         
