5d0d0e657a877cbe7e113d4d7bc398b6a3b7875f
C++_Algo <129144512+Aaditya20020109@users.noreply.github.com>
2024-05-04 22:40:08 +0530

    Time: 52 ms (82.35%) | Memory: 45.3 MB (85.44%) - LeetSync

---
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int boats = 0;
+        int n = people.size();
+        int i =0, j=n-1;
+        sort(begin(people),end(people));
+        while(i <= j)
+        {
+            if(people[i] + people[j] <= limit)
+            {
+                i++;
+
+            }
+            j--;
+            boats++;
+
+        }
+        return boats;
+    }
+};
