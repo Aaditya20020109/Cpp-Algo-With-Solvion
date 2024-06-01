a0f2ec194f427ae1e45dc93333913177cbd29f49
C++_Algo <129144512+Aaditya20020109@users.noreply.github.com>
2024-06-01 12:54:23 +0530

    Time: 6 ms (27.27%) | Memory: 7.9 MB (7.39%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.cpp
+++ 3379-score-of-a-string/score-of-a-string.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int scoreOfString(string s) {
-        vector<int> v;
-        v.push_back((int)s[0]);
-        int ans = 0;
-        for(int i=1 ;i<s.size() ; i++)
-        {
-            int curr = (int)s[i];
-            int prev = v[v.size()-1];
-            ans+=(abs(curr-prev));
-            v.push_back(curr);
-        }
+       int i =0,j=1;
+       int ans = 0;
+       while(j<s.size())
+       {
 
-        return ans;
+        ans+=abs((int)s[i] - (int)s[j]);
+        i++;
+        j++;
+       }
+       return ans;
         
     }
 };
