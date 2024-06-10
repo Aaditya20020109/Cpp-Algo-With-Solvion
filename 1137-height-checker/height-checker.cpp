550181b81efdc4ffaa7722d0c3b86686a01f77d5
C++_Algo <129144512+Aaditya20020109@users.noreply.github.com>
2024-06-10 18:47:55 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (10.73%) - LeetSync

---
+++ 1137-height-checker/height-checker.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+
+        vector<int> newHeight  = heights;
+
+        sort(heights.begin() , heights.end());
+        int ans = 0;
+        for(int i=0 ; i<heights.size() ; i++)
+        {
+            if(heights[i] != newHeight[i] )ans++;
+        }
+
+
+        return ans;
+        
+    }
+};
