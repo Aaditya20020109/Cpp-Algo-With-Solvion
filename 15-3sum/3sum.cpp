d23bb2dfd281779760481068615b70ff16620afe
C++_Algo <129144512+Aaditya20020109@users.noreply.github.com>
2024-08-13 22:13:29 +0530

    Time: 59 ms (75.56%) | Memory: 27.2 MB (82.63%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,40 +1,42 @@
-#include <vector>
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        // test case{-2 , 0 , 0 , 2 , 2}
         vector<vector<int>> res;
-        sort(nums.begin(), nums.end());
-        int i = 0;
+        sort(nums.begin() , nums.end());
         int n = nums.size();
-        for (i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
+        for(int i=0 ; i<n-2 ; i++)
+        {
+            if(i > 0 && nums[i-1]==nums[i]){
                 continue;
-            else {
-                int j = i + 1;
-                int k = n - 1;
-                while (j < k) {
-                    int sum = nums[i] + nums[j] + nums[k];
-                    if (sum == 0) {
-                        vector<int> v = {nums[i], nums[j], nums[k]};
-                        if (res.size() > 0) {
-                            int m = res.size();
-                            if (res[m - 1] != v) {
-                                res.push_back(v);
-                            }
-                        } else {
-                            res.push_back(v);
-                        }
+            }
 
-                        k--;
-                        j++;
-                    }
-                    if (sum > 0)
-                        k--;
-                    if (sum < 0)
-                        j++;
+            int j = i+1 , k = n-1;
+            while(j<k)
+            {
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum == 0)
+                {
+                    res.push_back({nums[i],nums[j] , nums[k]});
+                     j++;
+                     while(j > i+1 && nums[j] == nums[j-1] && j<k )j+=1;
+                    //  k-=1;
+                  
                 }
+                else{
+                    if(sum > 0)
+                    {
+                        k-=1;
+                    }else{
+                        j+=1;
+                    }
+                }
+
+
+                 
             }
         }
         return res;
+        
     }
 };
