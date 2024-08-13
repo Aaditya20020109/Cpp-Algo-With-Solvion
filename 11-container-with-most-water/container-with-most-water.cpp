7bf2ecda614fb69ce1f91e1c36fa8ae2ef76e4b0
C++_Algo <129144512+Aaditya20020109@users.noreply.github.com>
2024-08-13 23:31:52 +0530

    Time: 70 ms (15.24%) | Memory: 61.8 MB (16.69%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,39 +1,28 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int n = height.size();
-        int maxi =0;
-        vector<int> left(n);
-        vector<int> right(n);
-        left[0] =  height[0];
-        right[n-1] = height[n-1];
-        for(int i=1;i<n;i++)
-        {
-            left[i] = max(left[i-1],height[i]);
-            cout<<left[i]<<" ";
-        }
-    cout<<endl;
-        for(int i=n-2;i>=0;i--)
-        {
-            right[i] = max(right[i+1],height[i]);
-            cout<<right[i]<<" ";
-        }
-        int i=0;
-        int j=n-1;
-        while(i<=j)
-        {
-            int height = min(left[i],right[j]);
-            int width = abs(i-j);
-            int area = height*width;
-            maxi = max(area,maxi);
-            if(left[i]<right[j])i++;
-            else if(left[i]>right[j])j--;
-           else { i++;
-            j--;}
+        // two pointer
+        int maxArea = 0 ;
+        int i =0 ;
+        int j = height.size() - 1;
 
+        while(i<j)
+        {
+            int height1 = min(height[i] , height[j]);
+            int width = abs(j-i);
+            int area = height1 * width;
+            maxArea = max(maxArea , area);
+            if(height[i]>height[j])
+            {
+                j-=1;
+            }
 
+           else{
+                i+=1;
+           }
         }
-        return maxi;
+
+        return maxArea;
         
     }
 };
