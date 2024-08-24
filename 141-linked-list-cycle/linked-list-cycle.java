7b6abb164361c191a4dcd0992a09957a8eac57ae
C++_Algo <129144512+Aaditya20020109@users.noreply.github.com>
2024-08-24 21:31:51 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (35.95%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,21 +11,29 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head==null || head.next==null)
+        if(head == null ||head.next == null)return false;
+        ListNode fast = head.next;
+        ListNode slow = head;
+        boolean is = false;
+        while(slow!=fast)
         {
-            return false;
-        }
+             if(fast == null || fast.next == null)
+            {
+                return false;
+               
+            }
 
-        ListNode slow = head,fast=head;
-        while( fast!=null  && fast.next!=null )
-        {
             slow = slow.next;
             fast = fast.next.next;
-            if(slow==fast)
-            {
-                return true;
-            }
+
+           
+
         }
-        return false;
+
+
+       
+
+        return true;
+        
     }
 }
