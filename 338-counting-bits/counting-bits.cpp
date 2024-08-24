f1eccfc14e7034bfb4ac71d66d202fb8b48decec
C++_Algo <129144512+Aaditya20020109@users.noreply.github.com>
2024-08-24 08:32:07 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (24.81%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,35 +1,17 @@
 class Solution {
 public:
-
-int hammingWeight(int n) {
-        int count = 0;
-        for(int i =0 ; i<32 ; i++)
-        {
-            int ith_bitvalue = (1&(n>>i));
-            if(ith_bitvalue == 1)
-            {
-                count+=1;
-            }
-
-
-        }
-        return count;
-        
-    }
     vector<int> countBits(int n) {
-        vector<int> ans;
-        int i = 0 ;
 
+        vector<int> ans ;
+        int  i =0;
         while(i<=n)
         {
-            int total_bits = hammingWeight(i)
-            ;
+            int total_bits = __builtin_popcount(i);
             ans.push_back(total_bits);
+            i+=1;
 
-            i++;
         }
-
-        return ans ;
+        return ans;
         
     }
 };
