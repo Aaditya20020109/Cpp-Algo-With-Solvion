f9feeb8f277e56f2e3ac7ba54017d5eb83be2a2e
C++_Algo <129144512+Aaditya20020109@users.noreply.github.com>
2024-10-22 23:09:44 +0530

    Time: 15 ms (54.61%) | Memory: 12.7 MB (48.22%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int i=0,j=0;
-        int n=s.size();
-        int maxi =0;
-        map<char,int> m;
-        for(j=0;j<n;j++)
+      unordered_map<char , int> m;
+      int max_len = 0 , i=0;
+      for(int j=0 ; j<s.size() ; j++)
+      {
+        m[s[j]]++;
+        while(m[s[j]] > 1)
         {
-            char ch = s[j];
-            if(m.find(ch)!=m.end())
-            {
-                m[ch]++;
-            }
-            else {
-                m.insert({ch,1});
-            }
+          m[s[i]]--;
+          if(m[s[i]] == 0)
+          {
+            m.erase(s[i]);
 
-            while(m[ch]>1)
-            {
-                char ch = s[i];
-                m[ch]--;
-                if(m[ch]==0)m.erase(ch);
-                i++;
-            }
-            maxi = max(maxi,(j-i+1));
+          }
+          i++;
+          
         }
-        return maxi;
+        max_len = max(max_len , j-i+1);
+        
+
+      }
+      return max_len;
         
     }
 };
