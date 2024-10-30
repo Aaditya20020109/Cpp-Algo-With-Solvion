10d18f2db199bb58b7fefd8ef4b0c2b8fb625b8b
C++_Algo <129144512+Aaditya20020109@users.noreply.github.com>
2024-10-30 18:02:21 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (98.95%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,11 +1,43 @@
 class Solution {
 public:
+
+    void side_rotate(vector<int>& nums , int start , int end)
+    {
+        int i = start,j = end;
+        while(i<j)
+        {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+
+    }
     void rotate(vector<int>& nums, int k) {
-        k = k%nums.size();
-        int start = nums.size()-k;
-        reverse(begin(nums),begin(nums)+start);
-        reverse(begin(nums)+start,end(nums));
-        reverse(begin(nums),end(nums));
+
+    while(nums.size() < k)
+       {
+       
+        k = k-nums.size();
+
+       }
+
+        // first reverse from left
+        int i = nums.size()-k;
+        int j = nums.size()-1;
+        side_rotate(nums,i,j);
+
+        i=0 , j = nums.size()-k-1;
+         side_rotate(nums,i,j);
+       
+
+        i = 0  , j=nums.size()-1;
+        side_rotate(nums,i,j);
+
+       
+
+
         
     }
 };
