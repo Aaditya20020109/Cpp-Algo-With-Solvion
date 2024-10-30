0aaeaeb32cbeb44dd29b87c934d31b2733351ab8
C++_Algo <129144512+Aaditya20020109@users.noreply.github.com>
2024-10-30 17:33:16 +0530

    Time: 15 ms (82.16%) | Memory: 25.3 MB (35.84%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
+
+string generate(string s)
+{
+    int ch[26] = {0};
+    for(char &ch_1:s)
+    {
+        ch[ch_1-'a']++;
+    }
+
+    string res = "";
+    for(int i=0 ; i<26 ; i++)
+    {
+        if(ch[i]>0)
+        {
+            res+=string(ch[i] , i+'a');
+
+        }
+    }
+
+    return res;
+}
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         
-        vector<vector<string>> res ;
+        vector<vector<string>> res;
         unordered_map<string , vector<string>> mp;
+        
         for(string s:strs)
         {
-            string value = s;
-            sort(s.begin() , s.end());
-            if(mp.find(s)!=mp.end())
-            {
-                mp[s].push_back(value);
-                
-
-            }else{
-                mp.insert({s , {value}});
-
-            }
+            string res_1 = generate(s);
+            mp[res_1].push_back(s);
         }
 
 
-        for(auto  it=mp.begin() ; it!=mp.end() ; it++)
+        for(auto it=mp.begin(); it!=mp.end() ; it++)
         {
             res.push_back(it->second);
         }
