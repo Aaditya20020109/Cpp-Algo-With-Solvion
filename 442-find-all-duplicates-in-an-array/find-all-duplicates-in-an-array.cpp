225410668c6be923c70482c82f5d418efcf72470
C++_Algo <129144512+Aaditya20020109@users.noreply.github.com>
2024-11-01 07:39:53 +0530

    Time: 15 ms (49.42%) | Memory: 37.3 MB (49.40%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        // TC:O(n)
-        // SC:O(1)
-        sort(begin(nums),end(nums));
+
         vector<int> res;
-        int i=0;
-        int n = nums.size();
-        while(i<n-1)
+        sort(begin(nums) , end(nums));
+        for(int i=0 ; i<nums.size()-1 ; i++)
         {
             if(nums[i]==nums[i+1])
             {
                 res.push_back(nums[i]);
-                i+=2;
-            }
-            else{
-                i+=1;
             }
 
         }
