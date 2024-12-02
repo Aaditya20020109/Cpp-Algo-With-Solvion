86f0723de4cc40a140faf82e1f35d49e8560dbb6
C++_Algo <129144512+Aaditya20020109@users.noreply.github.com>
2024-12-02 07:57:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (6.42%) - LeetSync

---
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+vector<string> v;
+int helper(string s1 , string s2)
+{
+v.push_back(s1);
+for(auto it:v)
+    {
+        cout<<it;
+        cout<<" ";
+    }
+    cout<<endl;
+if(s1.size() < s2.size())return -1;
+
+int i=0 , j = s2.size();
+
+if(s1.substr(i,j) == s2)return 1;
+
+return 0;
+
+}
+int isPrefixOfWord(string sentence, string searchWord) {
+int i = 0 , j = i;
+
+int n = sentence.size();
+int index = 1;
+
+while(i<n)
+{
+    j = i;
+    while(j<n && sentence[j]!=' ')
+    {
+        j++;
+
+    }
+    
+    if(helper(sentence.substr(i , j) , searchWord) == 1)
+    {
+        return index;
+
+    }
+    i = j+1;
+    index++;
+
+    
+
+}
+
+
+return -1;
+
+}
+};
