ad6bc742780be64fa90846e87d33a53667b945d2
C++_Algo_With_Solvion <129144512+Aaditya20020109@users.noreply.github.com>
2024-12-20 06:40:27 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (7.22%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
@@ -1,46 +1,32 @@
 class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
-
+        
         int n = arr.size();
-        vector<int> maxArr(n,0);
-        vector<int> minArr(n , 0);
 
-        maxArr[0] = arr[0];
-        minArr[n-1] = arr[n-1];
-
-        int count = 1;
-        int maxEle = arr[0];
-        for(int i=1;  i<n ; i++)
+        int sum = 0;
+        vector<int> v(n,0);
+        for(int i=0 ; i<n ; i++)
         {
-            maxEle = max(arr[i] , maxEle);
-            maxArr[i] = max(arr[i]  , maxEle);
+            sum+=arr[i];
+            v[i] = sum;
 
         }
+        int count =  0;
 
-        int minEle = arr[n-1];
-        for(int i = n-2 ; i>=0 ; i--)
+        sort(arr.begin() , arr.end());
+        int EachIndexSum = 0;
+        for(int i=0 ; i<n ; i++)
         {
-            minEle = min(arr[i] , minEle);
-            minArr[i] = min(arr[i]  , minEle);
-
-        }
-
-        for(int i=1 ; i<n ; i++)
-        {
-            int prefixMaxEle = maxArr[i-1];
-            int suffixMinEle = minArr[i];
-            if(prefixMaxEle < suffixMinEle )
+            EachIndexSum+=arr[i];
+            if(EachIndexSum == v[i])
             {
-                count-=1;
-                count+=2;
-
+                count++;
             }
+
         }
 
         return count;
 
-
-        
     }
 };
