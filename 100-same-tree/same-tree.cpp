544b61ba9c49714fb910b3e3b267c8804a77fcdc
C++_Algo_With_Solvion <129144512+Aaditya20020109@users.noreply.github.com>
2024-12-24 06:27:28 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (21.07%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -12,11 +12,19 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        // DFS
-        if(p==NULL && q==NULL)return true;
-        if(p==NULL || q==NULL)return false;
-        if((p->val!=q->val))return false;
-        return (isSameTree(p->left,q->left) && isSameTree(p->right,q->right));
+
+        if(p==NULL && q==NULL)
+        return true;
+
+        if(p==NULL || q==NULL)
+        return false;
+
+        if(p->val != q->val)
+        return false;
+
+       
+
+        return (isSameTree(p->left , q->left) && isSameTree(p->right , q->right));
         
     }
 };
