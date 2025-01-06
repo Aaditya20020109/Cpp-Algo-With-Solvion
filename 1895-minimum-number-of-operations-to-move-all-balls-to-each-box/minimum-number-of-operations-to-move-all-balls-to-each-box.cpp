359de45802310de63c8d0317be5c05a149329659
C++_Algo_With_Solvion <129144512+Aaditya20020109@users.noreply.github.com>
2025-01-06 07:37:49 +0530

    Time: 3 ms (63.60%) | Memory: 13.1 MB (6.09%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -2,32 +2,44 @@ class Solution {
 public:
     vector<int> minOperations(string boxes) {
 
-
-        vector<int> ones_idx;
-
         int n = boxes.size();
 
-        for(int i=0 ; i<n ; i++)
-        {
-            if(boxes[i] == '1')
-            {
-                ones_idx.push_back(i);
-            }
+        vector<int> leftCount(n, 0);
+        vector<int> rightCount(n, 0);
+
+        for (int i = 1; i < n; i++) {
+            leftCount[i] += leftCount[i - 1];
+            if (boxes[i - 1] == '1')
+                leftCount[i]++;
         }
 
-
-        vector<int> res(n,0);
-
-        for(int i=0 ; i<n ; i++)
-        {
-            for(int j=0 ; j<ones_idx.size() ; j++)
-            {
-                res[i] += abs(i-ones_idx[j]);
-            }
+        for (int i = n - 2; i >= 0; i--) {
+            rightCount[i] += rightCount[i + 1];
+            if (boxes[i + 1] == '1')
+                rightCount[i] += 1;
         }
 
+      
+
+        vector<int> left(n, 0);
+        vector<int> right(n, 0);
+
+        vector<int> res(n, 0);
+
+        // left
+        for (int i = 1; i < n; i++) {
+            left[i] = left[i - 1] + leftCount[i];
+        }
+
+        // right
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = right[i + 1] + rightCount[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            res[i] = left[i] + right[i];
+        }
 
         return res;
-        
     }
 };
