b11aae49f008200075fb250a5065d43dd8d8f74c
C++_Algo_With_Solvion <129144512+Aaditya20020109@users.noreply.github.com>
2025-02-01 14:10:43 +0530

    Time: 21 ms (44.38%) | Memory: 71.5 MB (41.07%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        unordered_map<int,int> people_trust;
-        unordered_map<int,int> judge_trust;
-        for(int i=1;i<=n;i++)
-        {
-            people_trust.insert({i,0});
-            judge_trust.insert({i,0});
 
-        }
-        for(int i=0;i<trust.size();i++)
+        if(n==1 && trust.size()==0)return 1;
+
+        unordered_map<int  , vector<int>> graph;
+        unordered_map<int , int> MosttrustedPersons;
+        for(auto& v:trust)
         {
-            people_trust[trust[i][1]]++; // ese judge jinpe bharosa kiya ja raha hai.
-            judge_trust[trust[i][0]]++;// judge kine bharosa karta hai.
+            int ai = v[0] , bi = v[1];
+            graph[ai].push_back(bi);
+            MosttrustedPersons[bi]++;
         }
 
 
-        // people_trust == n-1 of any element
-        // judge_trust  ==  0 of any element
-        unordered_map<int,int> :: iterator it;
-        for(it=people_trust.begin();it!=people_trust.end();it++)
+        for(auto it=MosttrustedPersons.begin() ; it!=MosttrustedPersons.end() ; it++)
         {
-            if(it->second==n-1 && judge_trust[it->first]==0)return it->first;
+            if(it->second == n-1 && !graph.count(it->first))
+            {
+
+                return it->first;
+
+            }
         }
+
         return -1;
 
 
+        
     }
 };
