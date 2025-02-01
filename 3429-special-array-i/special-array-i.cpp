4a18604c7068c29816496e511d4bf4cda15231a0
C++_Algo_With_Solvion <129144512+Aaditya20020109@users.noreply.github.com>
2025-02-01 13:50:11 +0530

    Time: 0 ms (100.00%) | Memory: 27.6 MB (61.71%) - LeetSync

--- 3429-special-array-i/special-array-i.cpp
+++ 3429-special-array-i/special-array-i.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
 
-        for(int i=1 ; i<nums.size() ; i++)
+        for(int i=0 ; i<nums.size()-1 ; i++)
         {
-            if(nums[i]%2!=0 && nums[i-1]%2!=0)return false;
-            if(nums[i]%2==0 && nums[i-1]%2==0)return false;
+            if(nums[i]%2!=0 && nums[i+1]%2!=0)return false;
+            if(nums[i]%2==0 && nums[i+1]%2==0)return false;
             
         }
 
