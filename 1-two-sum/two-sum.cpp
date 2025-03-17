c1283c65ce977937ab3898cf79bdf214c629c38b
C++_Algo_With_Solvion <129144512+Aaditya20020109@users.noreply.github.com>
2025-03-17 07:47:15 +0530

    Time: 3 ms (73.29%) | Memory: 15.7 MB (10.31%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
-        unordered_map<int , int> m;
+        unordered_map<int , vector<int>> m;
+
         for(int i=0 ; i<nums.size() ; i++)
         {
-            int x = target - nums[i];
-            if(m.count(x))
+            if(m.count(target-nums[i]))
             {
-                return {m[x] , i};
-            }
-            m[nums[i]] = i;
-        }
-        return {-1};
+                vector<int> res;
+                vector<int> point = m[target-nums[i]];
+                int index1 = i , index2 = point[point.size() - 1];
+                res = {index1 , index2};
+                return res;
 
+
+
+            }
+            m[nums[i]].push_back(i);
+        }
+        return {-1,-1};
+        
     }
 };
